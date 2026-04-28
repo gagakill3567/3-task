746b9786215fb110e8f258d8e939fc043eddf465
gagakill3567 <152619361+gagakill3567@users.noreply.github.com>
2026-04-28 08:54:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
+#include <stdio.h>
 #include "Train.h"
+
 int main()
 {
-    Train train("Tver", 2, 14);
-    Train train1("Moscow", 3, 15);
-    train.number = 10;
-    train1.number = 10;
-    std::cout << train.station << " " << train.number << " " << train.time;
-    std::cout << train1.station << " " << train1.number << " " << train1.time;
+    Train tr1("Тверь", 2, 14);
+    Train tr2("Москва", 3, 15);
+    
+    tr1.number = 10;
+    tr2.number = 10;
+
+    printf("Станция: %s, Номер: %d, Время: %d\n", tr1.station, tr1.number, tr1.time);
+    printf("Станция: %s, Номер: %d, Время: %d\n", tr2.station, tr2.number, tr2.time);
+
+    return 0;
 }
